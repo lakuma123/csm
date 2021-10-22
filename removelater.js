0a76a49eda3d2c1aeba2c49416f6750ab6d8a038
lakuma123 <68697370+lakuma123@users.noreply.github.com>
2021-10-22 20:37:20 +0530

    Update removelater.js

--- removelater.js
+++ removelater.js
@@ -5,7 +5,7 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
 export default function SelectVariants() {
-  const [SSIM, setAge] = React.useState('');
+  const [age, setAge] = React.useState('');
 
   const handleChange = (event) => {
     setAge(event.target.value);
@@ -14,38 +14,23 @@ export default function SelectVariants() {
   return (
     <div>
       <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="demo-simple-select-standard-label">SSIM</InputLabel>
+        <InputLabel id="demo-simple-select-standard-label">Age</InputLabel>
         <Select
           labelId="demo-simple-select-standard-label"
           id="demo-simple-select-standard"
-          value={SSIM1,SSIM2,SSIM3}
+          value={age}
           onChange={handleChange}
-          label="SSIM"
+          label="Age"
         >
           <MenuItem value="">
             <em>None</em>
           </MenuItem>
-          <MenuItem value={SSIM1}></MenuItem>
-          <MenuItem value={SSIM2}></MenuItem>
-          <MenuItem value={SSIM3}></MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
         </Select>
       </FormControl>
-      <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="demo-simple-select-filled-label">SSIM</InputLabel>
-        <Select
-          labelId="demo-simple-select-filled-label"
-          id="demo-simple-select-filled"
-          value={SSIM}
-          onChange={handleChange}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={SSIM1}></MenuItem>
-          <MenuItem value={SSIM2}></MenuItem>
-          <MenuItem value={SSIM3}></MenuItem>
-        </Select>
-      </FormControl>
+      
     </div>
   );
 }
